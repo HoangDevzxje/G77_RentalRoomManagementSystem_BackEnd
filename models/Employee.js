e9e53620f2ba62adeb5bcb054753523eb218e110
sieunhandong <dongnvhe172730@fpt.edu.vn>
2025-11-12 15:23:37 +0700

    fix model employee

--- models/Employee.js
+++ models/Employee.js
@@ -1,27 +1,26 @@
 const mongoose = require("mongoose");
 
 const employeeSchema = new mongoose.Schema(
-  {
-    accountId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Account",
-      required: true,
+    {
+        accountId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Account",
+            required: true,
+        },
+        landlordId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Account",
+            required: true,
+        },
+        assignedBuildings: [
+            { type: mongoose.Schema.Types.ObjectId, ref: "Building" },
+        ],
+        permissions: [{ type: String }],
+        isDeleted: { type: Boolean, default: false },
     },
-    landlordId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Account",
-      required: true,
-    },
-    assignedBuildings: [
-      { type: mongoose.Schema.Types.ObjectId, ref: "Building" },
-    ],
-    permissions: [{ type: String }],
-    isDeleted: { type: Boolean, default: false },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 employeeSchema.index({ landlordId: 1, isDeleted: 1 });
-employeeSchema.index({ accountId: 1 }, { unique: true });
 
 module.exports = mongoose.model("Employee", employeeSchema);
